a1fe7545e8a82762d85a278b9b1f7783a174b525
Kirti Mobikwik <kirti.nagpal@mobikwik.com>
2021-01-08 14:16:08 +0530

    Working Code for microservice Country

--- src/main/java/com/mobikwik/country/CountryApplication.java
+++ src/main/java/com/mobikwik/country/CountryApplication.java
@@ -2,12 +2,11 @@ package com.mobikwik.country;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 
 
 @SpringBootApplication
-@EnableEurekaClient
 public class CountryApplication {
     public static void main(String[] args) {
         SpringApplication.run(CountryApplication.class, args);
